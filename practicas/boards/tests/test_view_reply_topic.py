c7d87af5cb367d67962e16cbd195ccfd8ff518ff
Mario <marioleyvagil@outlook.com>
2021-01-25 21:40:26 -0800

    Sixth module
    Pagination + gravatar + updated views

--- practicas/boards/tests/test_view_reply_topic.py
+++ practicas/boards/tests/test_view_reply_topic.py
@@ -62,11 +62,12 @@ class SuccessfulReplyTopicTests(ReplyTopicTestCase):
         self.client.login(username=self.username, password=self.password)
         self.response = self.client.post(self.url, {'message': 'hello, world!'})
 
-    def test_redirection(self):
+   def test_redirection(self):
         '''
         A valid form submission should redirect the user
         '''
-        topic_posts_url = reverse('topic_posts', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk})
+        url = reverse('topic_posts', kwargs={'pk': self.board.pk, 'topic_pk': self.topic.pk})
+        topic_posts_url = '{url}?page=1#2'.format(url=url)
         self.assertRedirects(self.response, topic_posts_url)
 
     def test_reply_created(self):
